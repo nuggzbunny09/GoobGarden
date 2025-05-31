a652ebd5d4057d503fb144676d0c4c96da7570ec
nuggzbunny09 <86770960+nuggzbunny09@users.noreply.github.com>
2025-05-31 15:58:47 -0700

    Update itemData.js

--- itemData.js
+++ itemData.js
@@ -1,11 +1,11 @@
 const itemDirectory = {
-  Tree: {
+  tree: {
     name: "Tree",
     image: "images/Tree.png",
     description: "Acts as a barrier.",
     placeable: true
   },
-  Water: {
+  water: {
     name: "Water",
     image: "images/Water.png",
     description: "Goobs can drown, be careful.",
