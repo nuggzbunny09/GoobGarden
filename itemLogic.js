21f7a67c8ebf1c10abae99459ac43e716b206e6a
nuggzbunny09 <86770960+nuggzbunny09@users.noreply.github.com>
2025-05-31 14:43:15 -0700

    Create itemLogic.js

---
+++ itemLogic.js
@@ -0,0 +1,7 @@
+function placeItem(itemId, x, y) {
+  // Logic to place the item on the grid at (x, y)
+}
+
+function getItemEffect(itemId, goob) {
+  // Logic to determine how the item affects the goob (if at all)
+}
