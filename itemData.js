057b28ecfeb79115463e0656fc3caa4928e8d211
nuggzbunny09 <86770960+nuggzbunny09@users.noreply.github.com>
2025-05-31 14:42:40 -0700

    Create itemData.js

---
+++ itemData.js
@@ -0,0 +1,14 @@
+const itemDirectory = {
+  tree: {
+    name: "Tree",
+    image: "images/Tree.png",
+    description: "Provides shade and boosts mood.",
+    placeable: true
+  },
+  water: {
+    name: "Water",
+    image: "images/Water.png",
+    description: "Hydrates nearby Goobs.",
+    placeable: true
+  }
+};
