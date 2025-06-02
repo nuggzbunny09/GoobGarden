7e5d59e2d1538d1a4fcafd4dcfebf419d9784aab
nuggzbunny09 <86770960+nuggzbunny09@users.noreply.github.com>
2025-06-01 21:57:51 -0700

    Update script.js

--- script.js
+++ script.js
@@ -760,7 +760,6 @@ document.addEventListener('mouseup', (e) => {
   // If dropped anywhere else (not canvas), cancel drag
   if (!isInsideCanvas) {
     cleanupDragging();
-  }
 });
 
 function movePlacedItem(item, newX, newY) {
