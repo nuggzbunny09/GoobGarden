79a9ca8f6b9c643d3a37d246fdc076e8df533257
nuggzbunny09 <86770960+nuggzbunny09@users.noreply.github.com>
2025-06-01 17:34:05 -0700

    Update itemData.js

--- itemData.js
+++ itemData.js
@@ -3,17 +3,20 @@ const itemDirectory = {
     name: "Tree",
     image: "images/Tree.png",
     description: "Acts as a barrier.",
+    imageCenter: { x: 20, y: 20 },
     placeable: true
   },
   water: {
     name: "Water",
     image: "images/Water.png",
     description: "Goobs can drown, be careful.",
+    imageCenter: { x: 20, y: 20 },
     placeable: true
   },
   redBerry: {
     name: "Red Berry",
     image: "images/RedBerry.png",
     description: "Fills Goob's Tummy 8 points",
+    imageCenter: { x: 20, y: 20 },
     placeable: true
 };
